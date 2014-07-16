da74193b619f99d13d6d2f9496668b5ef78bc982
Claus Hunsen <hunsen@fim.uni-passau.de>
2014-07-16 10:40:26 +0200

    deletion of include guards now removes include guards as always, but inserts empty lines for the include guard. additionally, line endings are preserved, empty lines are not removed directly, and lines are not trimmed.

--- delete_include_guards.py
+++ delete_include_guards.py
@@ -9,7 +9,7 @@
 # #endif // foo
 #
 # parser works with following characteristics:
-#  - the include guard doesn't have to start at the first line
+# - the include guard doesn't have to start at the first line
 #    and doesn't have to finish at the last line
 #  - only one guard per file
 #  - deleting first include-guard of following syntax
@@ -23,7 +23,8 @@ import os, re, sys
 
 __debug = False
 
-def apply(fname):
+
+def apply(fname, out=sys.stdout):
     fname = os.path.abspath(fname)
 
     # check for include-guard
@@ -31,11 +32,17 @@ def apply(fname):
     rd = re.compile('#define\s+(\S+)')
     sourcecode = list()
 
-    with open(fname, 'r') as fd:
-        for line in fd.readlines():
-            l = line.strip()
-            if l:
-                sourcecode.append(l)
+    fd = open(fname, 'rU')
+
+    first_line = fd.readline()  # read first line to determine line separator
+    eol = fd.newlines
+    fd.seek(0)
+
+    sourcecode = map(str.strip, fd.readlines())
+    # for line in fd:
+    #     l = line.strip()
+    #     if l:
+    #         sourcecode.append(l)
 
     def _findCorrespondingItems(sourcecode):
         '''This method returns a tuple with the include guard elements to cut of the source.
@@ -61,7 +68,7 @@ def apply(fname):
                 ifdef = item
                 ifdefpos = currentitem
 
-                 # processing ifdef
+                # processing ifdef
                 ifdefexpr = rg.match(ifdef)
                 if (ifdefexpr):
                     # potential include-guard matched
@@ -89,20 +96,20 @@ def apply(fname):
 
         # process taillist for else and endif
         ifcount = 1
-        currentitem = 1 # go two steps ahead as we jump over #if and #define
+        currentitem = 1  # go two steps ahead as we jump over #if and #define
         for item in taillist[2:]:
             currentitem += 1
             if item.startswith('#else') and ifcount == 1:
-                return (-1, -1)        # we do not support alternative include guards
+                return (-1, -1)  # we do not support alternative include guards
             if item.startswith('#elif') and ifcount == 1:
-                return (-1, -1)        # we do not support alternative include guards
+                return (-1, -1)  # we do not support alternative include guards
             if item.startswith('#if'):
                 ifcount += 1
                 continue
             if item.startswith('#endif'):
                 ifcount -= 1
                 if ifcount == 0:
-                    return(ifdefpos, currentitem+ifdefpos)
+                    return (ifdefpos, currentitem + ifdefpos)
                 continue
         return (-1, -1)
 
@@ -110,12 +117,16 @@ def apply(fname):
     if (ifdef == -1 or endif == -1):
         pass
     else:
-        # sourcecode = before include guard + within include guard (without guard and define) + after include guard
-        sourcecode = sourcecode[:ifdef] + sourcecode[ifdef+2:endif] + sourcecode[endif+1:]
+        # sourcecode = before include guard + within include guard (without guard and define (but empty lines instead)) + after include guard
+        emptyItemsList = [i for i in ("","","")]
+        sourcecode = sourcecode[:ifdef] \
+                     + emptyItemsList[0:2] \
+                     + sourcecode[ifdef + 2:endif] \
+                     + emptyItemsList[:2] \
+                     + sourcecode[endif + 1:]
 
     for item in sourcecode:
-        print(item)
-
+        out.write(item + eol)
 
 
 def usage():

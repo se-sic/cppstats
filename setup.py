75301853d1f3d9d75c0d66b845bd1e8d3e97ee40
Claus Hunsen <hunsen@fim.uni-passau.de>
2017-02-20 22:10:06 +0100

    Push version number to v0.9.3
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cppstats',
-    version="0.9.2",
+    version="0.9.3",
     packages=find_packages(exclude=['scripts']),
     url='http://www.fosd.net/cppstats',
     license='LGPLv3',

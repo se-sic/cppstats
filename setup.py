8d16ce31ebbe278008e1d3e9b03fda04ca34e183
Niklas Schneider <s8nlschn@stud.uni-saarland.de>
2023-07-05 11:45:41 +0200

    Fix more imports

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     name='cppstats',
     version="0.9.4",
     packages=find_packages(exclude=['scripts']),
-    package_dir={'cppstats': '.',},
     url='http://www.fosd.net/cppstats',
     license='LGPLv3',
     author='Claus Hunsen',
